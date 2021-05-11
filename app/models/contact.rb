4a6a9813fe55d2b412175503267a933b464aa587
friedrich <friedrichchiu@Friedrichs-MacBook-Air.local>
2021-05-11 11:09:46 +0800

    contact form built

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,8 @@
 class Contact < ApplicationRecord
+  validates :name, presence: true
+  validates :email, presence: true, format: { with: /.+\@.+\..+/ }
+  validates :tel, presence: true
+  validates :event, presence: true
+  validates :title, presence: true
+  validates :feedback, presence: true
 end

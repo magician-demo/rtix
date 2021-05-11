9d6baa16f3d1b4bf20299e15892a44bd4c4ba99f
friedrich <friedrichchiu@Friedrichs-MacBook-Air.local>
2021-05-11 08:02:46 +0800

    start building contact form

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  resources :dashboards, path: 'dashboard'
 
+  resources :dashboards, path: 'dashboard'
+  resources :contacts, only: [:new, :create]
   resources :admins, path: 'admin'
+
 
   root "events#index"
 end

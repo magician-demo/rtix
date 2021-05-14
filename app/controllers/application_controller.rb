0b2d76388bc609ee800d74c74db0af9a2c2ce184
RobertChang <fishppm0212@outlook.com>
2021-05-14 16:08:11 +0800

    add scss

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,5 @@
 class ApplicationController < ActionController::Base
-  
+  def user_cart
+    current_user.cart
+  end
 end
--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -2,5 +2,13 @@ class LineItemsController < ApplicationController
   before_action :authenticate_user!
 
   def create
+    cart = user_cart
+    seat = Seat.find(params[:seat_id])
+    line_item = cart.line_items.new(seat_id: seat.id)
+    if line_item.save
+      render json: {status: 'selected'}
+    else
+      render json: {status: 'error'}
+    end
   end
 end

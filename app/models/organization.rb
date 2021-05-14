9104af7c81dc46f73bf732f0d4636d2e0efdf316
SunWei Chou <sunwei8524@gmail.com>
2021-05-14 16:26:38 +0800

    新增: 組織軟刪除功能

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,4 +1,4 @@
 class Organization < ApplicationRecord
-
+  acts_as_paranoid #Soft Delete
   belongs_to :user
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_13_044455) do
+ActiveRecord::Schema.define(version: 2021_05_14_080001) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2021_05_13_044455) do
     t.integer "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["deleted_at"], name: "index_organizations_on_deleted_at"
   end
 
   create_table "users", force: :cascade do |t|

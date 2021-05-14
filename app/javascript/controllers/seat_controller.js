fdd445d29a29ace5c3b172a3fa4ce451d77f552b
RobertChang <fishppm0212@outlook.com>
2021-05-14 20:38:16 +0800

    即時刪除購物車物品

--- app/javascript/controllers/seat_controller.js
+++ app/javascript/controllers/seat_controller.js
@@ -3,6 +3,7 @@ import axios from 'axios'
 
 export default class extends Controller {
   addLineItem(){
+    let count = document.getElementById('ticket_count')
     let ax = axios.create()
     let token = document.querySelector('meta[name=csrf-token]').content
     const id = this.element.dataset['seatId']
@@ -11,6 +12,7 @@ export default class extends Controller {
       .then(res =>{
         if(res.data['status'] === 'selected'){
           this.element.classList.add('selected')
+          count.innerHTML = Number(count.textContent) + 1
         }
       })
       .catch(err =>{

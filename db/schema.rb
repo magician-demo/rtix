3403732eee326d60c1801cfbd3cc1a7718dca946
RobertChang <fishppm0212@outlook.com>
2021-05-16 23:47:51 +0800

    solve conflict

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,33 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_14_080001) do
+ActiveRecord::Schema.define(version: 2021_05_14_045333) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "carts", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_carts_on_user_id"
+  end
+
+  create_table "events", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "line_items", force: :cascade do |t|
+    t.bigint "cart_id", null: false
+    t.bigint "seat_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["cart_id"], name: "index_line_items_on_cart_id"
+    t.index ["seat_id"], name: "index_line_items_on_seat_id"
+  end
 
   create_table "organizations", force: :cascade do |t|
     t.string "title"
@@ -23,6 +46,26 @@ ActiveRecord::Schema.define(version: 2021_05_14_080001) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["deleted_at"], name: "index_organizations_on_deleted_at"
+  end
+
+  create_table "seats", force: :cascade do |t|
+    t.string "area"
+    t.string "status"
+    t.bigint "ticket_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["ticket_id"], name: "index_seats_on_ticket_id"
+  end
+
+  create_table "tickets", force: :cascade do |t|
+    t.string "name"
+    t.integer "price"
+    t.integer "amount"
+    t.string "status"
+    t.bigint "event_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["event_id"], name: "index_tickets_on_event_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -37,4 +80,9 @@ ActiveRecord::Schema.define(version: 2021_05_14_080001) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "carts", "users"
+  add_foreign_key "line_items", "carts"
+  add_foreign_key "line_items", "seats"
+  add_foreign_key "seats", "tickets"
+  add_foreign_key "tickets", "events"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,7 @@
 250.times do
   Seat.create(
-    area: '搖滾區',
-    ticket_id: '1'
+    area: 'A區',
+    ticket_id: '4',
+    status: 'for_sale'
   )
 end

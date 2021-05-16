fa198de275aeae32ccc941d736bf06598fef01ab
RobertChang <fishppm0212@outlook.com>
2021-05-16 23:30:49 +0800

    add seed for test

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,17 @@
+Event.create(
+  title: '演唱會',
+  description: '好棒'
+)
+Ticket.create(
+  name: '豪華票',
+  price: '1900',
+  amount: '250',
+  event_id: Event.first.id
+)
 250.times do
   Seat.create(
-    area: 'B區',
-    ticket_id: '5',
+    area: 'A區',
+    ticket_id: Ticket.first.id,
     status: 'for_sale'
   )
 end

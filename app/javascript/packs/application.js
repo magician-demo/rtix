0870011239583d3584c6b0d987cf02b91bc32109
friedrich <friedrichchiu@Friedrichs-MacBook-Air.local>
2021-05-18 14:36:33 +0800

    bootstrap/jquery/popperjs

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,18 +3,19 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
+import Rails, { $ } from "@rails/ujs"
+import "@hotwired/turbo-rails"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-import "stylesheets/application" 
+import "stylesheets" 
 import "icon"
+
+import "jquery"
+import "popper.js"
+import "bootstrap"
 
 
 Rails.start()
-Turbolinks.start()
 ActiveStorage.start()
 
 import "controllers"
-
-
--- app/javascript/stylesheets/tailwind.config.js
+++ app/javascript/stylesheets/tailwind.config.js
@@ -15,4 +15,7 @@ module.exports = {
     extend: {},
   },
   plugins: [],
+  corePlugins: {
+    preflight: false,
+   }
 }

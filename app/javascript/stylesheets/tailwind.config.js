502c92146c8ed2a989fffb27886187b777ce7647
SunWei Chou <sunwei8524@gmail.com>
2021-05-19 23:46:33 +0800

    新增: org 切版

--- app/javascript/stylesheets/tailwind.config.js
+++ app/javascript/stylesheets/tailwind.config.js
@@ -7,6 +7,11 @@ module.exports = {
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    height:{
+      "9/10": '90%',
+      "1/10": '10%',
+      "screen": '100vh'
+    },
     extend: {},
   },
   variants: {

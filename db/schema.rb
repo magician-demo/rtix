25316dfaf9d639e60ec7228d164d56bedcf8d1bf
SunWei Chou <sunwei8524@gmail.com>
2021-05-22 14:43:56 +0800

    暫時rollback

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_21_074419) do
+ActiveRecord::Schema.define(version: 2021_05_14_080001) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,17 +29,6 @@ ActiveRecord::Schema.define(version: 2021_05_21_074419) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table "friendly_id_slugs", force: :cascade do |t|
-    t.string "slug", null: false
-    t.integer "sluggable_id", null: false
-    t.string "sluggable_type", limit: 50
-    t.string "scope"
-    t.datetime "created_at"
-    t.index ["slug", "sluggable_type", "scope"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type_and_scope", unique: true
-    t.index ["slug", "sluggable_type"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type"
-    t.index ["sluggable_type", "sluggable_id"], name: "index_friendly_id_slugs_on_sluggable_type_and_sluggable_id"
-  end
-
   create_table "line_items", force: :cascade do |t|
     t.bigint "cart_id", null: false
     t.bigint "seat_id", null: false
@@ -56,11 +45,7 @@ ActiveRecord::Schema.define(version: 2021_05_21_074419) do
     t.integer "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "domain_name"
-    t.string "slug"
-    t.string "image"
     t.index ["deleted_at"], name: "index_organizations_on_deleted_at"
-    t.index ["slug"], name: "index_organizations_on_slug", unique: true
   end
 
   create_table "seats", force: :cascade do |t|

810b3bef6ea5336b22db6586eb39a9ba0a2c2a2d
RobertChang <fishppm0212@outlook.com>
2021-05-22 17:08:17 +0800

    根據訂票頁面顯示目前動作以及新增單一活動版面

--- app/models/seat.rb
+++ app/models/seat.rb
@@ -1,5 +1,6 @@
 class Seat < ApplicationRecord
   belongs_to :ticket
   has_one :line_item
-  after_commit { BookingStatusJob.perform_later(self) }
+  after_update_commit { BookingStatusJob.perform_later(self) }
+  
 end

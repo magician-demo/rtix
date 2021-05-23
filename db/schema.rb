922efc1758f6abe64834aceb82c12e0cb4b226a9
Weil J <jianweihsin@gmail.com>
2021-05-24 01:27:00 +0800

    user.rb-setup

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2021_05_20_115927) do
+ActiveRecord::Schema.define(version: 2021_05_23_165350) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -101,6 +100,8 @@ ActiveRecord::Schema.define(version: 2021_05_20_115927) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "provider"
+    t.string "uid"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end

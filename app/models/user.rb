3b67aa1a1312f8fc675723a7c5cdbae924ef2c60
Weil J <jianweihsin@gmail.com>
2021-05-24 12:30:13 +0800

    setup-omniauth-controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,20 @@ class User < ApplicationRecord
   
   has_many :organizations
   has_one :cart
+
+
+  # 找到user的話就登入，找不到就create新的user
+  def self.create_from_provider_data(provider_data)
+    where(email: provider_data.info.email).first_or_create do |user|
+      user.email = provider_data.info.email
+      user.password = Devise.friendly_token[0, 20]
+      user.name = provider_data.info.last_name
+      user.provider = provider_data.provider
+      user.uid = provider_data.uid
+    end
+  end
+  
 end
+
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   resources :organizations
-  devise_for :users
+
+  # 加入新的路徑”users/omniauth_callbacks”
+  # 去找omniauth_callbacks_controller
+  devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
   resources :events do
     resources :booking, only: [:index, :show]
   end
@@ -8,6 +11,19 @@ Rails.application.routes.draw do
     post '/line_items/random_create', to: 'line_items#random_create'
   resource :carts, only: [:destroy]
   root "events#index"
+
+  
+
+
+
+
+
+
+
+
+
+
+
 
   
   resource :cart, only: [:show, :destroy] do

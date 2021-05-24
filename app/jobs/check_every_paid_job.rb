35fee3266db521456d14919046a0ebfc0bafcf88
Skye <80411976+skyee2021@users.noreply.github.com>
2021-05-24 23:06:14 +0800

    Update check_every_paid_job.rb
    刪byebug

--- app/jobs/check_every_paid_job.rb
+++ app/jobs/check_every_paid_job.rb
@@ -5,7 +5,6 @@ class CheckEveryPaidJob < ApplicationJob
   def perform
     Order.all.each do |order|
       if order.status == "pending"
-        byebug
         order.cancel!
         order.seats.each do |seat|
           seat.update(status: "for_sale")

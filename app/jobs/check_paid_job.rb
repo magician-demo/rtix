4eeaaf90ad64ae3b2ab818ee61f2826e3345151e
Skye <80411976+skyee2021@users.noreply.github.com>
2021-05-26 21:35:23 +0800

    Update check_paid_job.rb
    這個不需要判斷

--- app/jobs/check_paid_job.rb
+++ app/jobs/check_paid_job.rb
@@ -2,7 +2,7 @@ class CheckPaidJob < ApplicationJob
   queue_as :default
 
   def perform(order)
-    if order.created_at <= 10.minutes.ago && order.status == 'pending'
+#     if order.created_at <= 10.minutes.ago && order.status == 'pending'
       order.cancel!
     end
   end

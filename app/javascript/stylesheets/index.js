55837f9daed127b765242d28e9a32485549c5dc2
RobertChang <fishppm0212@outlook.com>
2021-05-28 20:40:26 +0800

    最新版本解除衝突

--- app/javascript/stylesheets/index.js
+++ app/javascript/stylesheets/index.js
@@ -1,13 +1,5 @@
-<<<<<<< HEAD
 import "./application"
 import "./my_order"
 import "./booking"
 import "./events"
 import "./organization"
-=======
-import './application'
-import './my_order'
-import './booking'
-import './events'
-
->>>>>>> 07443aa75a4c8c6f718ed89665492d86bd326f38

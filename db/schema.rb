3d5833ae2ed22d5adf2b1b2aedd5807ee911226f
RobertChang <fishppm0212@outlook.com>
2021-05-30 21:46:39 +0800

    rebase dev and migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-
 ActiveRecord::Schema.define(version: 2021_05_30_074205) do
-=======
-ActiveRecord::Schema.define(version: 2021_05_28_155711) do
->>>>>>> 868d398 (add seed & area map)
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -67,12 +62,9 @@ ActiveRecord::Schema.define(version: 2021_05_28_155711) do
     t.string "e_time"
     t.string "city"
     t.bigint "organization_id"
-<<<<<<< HEAD
     t.datetime "start_time"
     t.datetime "end_time"
     t.string "image"
-=======
->>>>>>> 868d398 (add seed & area map)
     t.index ["organization_id"], name: "index_events_on_organization_id"
   end
 

34f29ff8f0ecd71905583d4505a16ba0adbc8442
friedrich <friedrichchiu@Friedrichs-MacBook-Air.local>
2021-06-01 12:26:03 +0800

    routes trimed/ process worked

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -1,21 +1,26 @@
 class TicketsController < ApplicationController
-  before_action :find_event
-  before_action :find_organization, except: [:update]
-
   def new
+    @event = Event.find(params[:event_id])
+    @organization = @event.organization
     @ticket = Ticket.new
   end
 
   def create
-    @ticket = @event.tickets.new(ticket_params)
-    @organization = current_user.organizations.find_by(user_id: current_user.id)
+    @event = Event.find_by(id: params[:event_id])
 
-    if @ticket.save
-      (@ticket.amount).times { Seat.create(area: (@ticket.name).to_s, ticket_id: @ticket.id, status: 'for_sale') }
-      redirect_to event_path(@event.id), notice: "創建成功！"
-    else
-      redirect_to events_organization_path(@event.id)
+    params.require(:tickets).each do |ticket|
+      @permited_ticket = ticket.permit(:name, :price, :amount).to_h  
+
+      #無法讀到ActionController::Parameters裡面的key的解決方法 就是用to_h轉成hash 腦洞開(https://stackoverflow.com/questions/34949505/rails-5-unable-to-retrieve-hash-values-from-parameter/34951198)
+
+      if @permited_ticket[:name].present? && @permited_ticket[:price].present? && @permited_ticket[:amount].present?
+        @ticket_record = @event.tickets.new(@permited_ticket)
+        @ticket_record.save 
+        (@ticket_record[:amount].to_i).times { Seat.create(area: (@ticket_record[:name]), ticket_id: @ticket_record[:id], status: 'for_sale') } 
+      end
     end
+
+    redirect_to event_path(params[:event_id]), notice: "活動創建成功!"
   end
 
   def edit
@@ -25,22 +30,8 @@ class TicketsController < ApplicationController
 
   def update
     @ticket = Ticket.find_by(id: params[:id])
-    
     @ticket.update(ticket_params)
-    
     redirect_to events_organization_path(@ticket.event.organization, @ticket.event.id), notice: "更新成功"
   end
 
-  private
-  def ticket_params
-    params.require(:ticket).permit(:name, :price, :amount)
-  end
-
-  def find_event
-    @event = current_user.organizations.find_by(user_id: current_user.id).events.find_by(id: params[:event_id])
-  end
-
-  def find_organization
-    @organization = current_user.organizations.find_by(user_id: current_user.id)
-  end
 end

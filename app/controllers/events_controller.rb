ad8c668c8355329c560ba88b6234c5f32070314e
friedrich <friedrichchiu@Friedrichs-MacBook-Air.local>
2021-06-01 17:42:40 +0800

    org's events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,6 +1,5 @@
 class EventsController < ApplicationController
   before_action :find_event, only: [:show, :edit, :update, :destroy]
-  before_action :find_organization, only: [:create, :edit, :update, :destroy]
 
   def index
     @events = Event.all
@@ -10,7 +9,7 @@ class EventsController < ApplicationController
   end
 
   def show
- 
+    # FIXME:這裏event似乎是重複寫了? 
     event = Event.find(params[:id])
     @tickets = Event.find(params[:id]).tickets.sort_by{ |ticket| ticket.id}
     @event = Event.find(params[:id])
@@ -24,42 +23,35 @@ class EventsController < ApplicationController
   def create
     @event = Event.new(event_params)
     if @event.save
-      redirect_to new_event_ticket_path(@event.id), notice: "請接著繼續設定此活動的票券"
+      redirect_to new_event_ticket_path(@event.id), notice: "請接著繼續設定活動的票券"
     else
       render :new
     end
   end
 
   def edit
-    # @organization = current_user.organizations.find_by(user_id: current_user.id)
-    # @event = current_event
+    @event = Event.find_by(id: params[:id])
   end
 
   def update
-    # @event = current_event
     @event.update(event_params)
-    
-    redirect_to events_organization_path(@organization), notice: "更新成功"
+    redirect_to events_organization_path(event_params[:organization_id]), notice: "更新成功"
   end
 
   def destroy
-    # @tickets.tickets.destroy_all
     @event.destroy
-    redirect_to events_organization_path(@organization), notice: "活動刪除成功！" 
+    redirect_to events_organization_path(params[:organization_id]), notice: "活動刪除成功！" 
   end
+
 
   private
   def event_params
     params.require(:event).permit(:title, :description, :location, :start_time, :end_time, :address, :image, :organization_id)
-    
   end
 
   def find_event
     @event = current_event
   end
 
-  def find_organization
-    @organization = current_user.organizations.find_by(user_id: current_user.id)
-  end
 end
 
--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -7,10 +7,8 @@ class TicketsController < ApplicationController
 
   def create
     @event = Event.find_by(id: params[:event_id])
-
     params.require(:tickets).each do |ticket|
       @permited_ticket = ticket.permit(:name, :price, :amount).to_h  
-
       #無法讀到ActionController::Parameters裡面的key的解決方法 就是用to_h轉成hash 腦洞開(https://stackoverflow.com/questions/34949505/rails-5-unable-to-retrieve-hash-values-from-parameter/34951198)
 
       if @permited_ticket[:name].present? && @permited_ticket[:price].present? && @permited_ticket[:amount].present?
@@ -19,7 +17,6 @@ class TicketsController < ApplicationController
         (@ticket_record[:amount].to_i).times { Seat.create(area: (@ticket_record[:name]), ticket_id: @ticket_record[:id], status: 'for_sale') } 
       end
     end
-
     redirect_to event_path(params[:event_id]), notice: "活動創建成功!"
   end
 

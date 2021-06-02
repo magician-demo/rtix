bd2754fd2c6852607e36fec52f9fcc8eb514fe31
RobertChang <fishppm0212@outlook.com>
2021-06-03 04:31:43 +0800

    add facebook & github log_in

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -11,6 +11,30 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     end
   end
 
+  def facebook
+    @user = User.create_from_provider_data(request.env["omniauth.auth"])
+
+    if @user.persisted?
+      flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "Facebook"
+      sign_in_and_redirect @user, :event => :authentication
+    else
+      session["devise.facebook_data"] = request.env["omniauth.auth"]
+      redirect_to new_user_registration_url
+    end
+  end
+
+  def github
+    @user = User.create_from_provider_data(request.env["omniauth.auth"])
+
+    if @user.persisted?
+      flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "Github"
+      sign_in_and_redirect @user, :event => :authentication
+    else
+      session["devise.github_data"] = request.env["omniauth.auth"]
+      redirect_to new_user_registration_url
+    end
+  end
+
   def failure
     redirect_to new_user_session_path, alert: "無法獲得驗證！"
   end

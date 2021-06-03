292ea9a379dc9af3bc44c7c9961166b1124fef53
Skye <skyee20211@gmail.com>
2021-06-03 21:55:06 +0800

    google map after address

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -1,20 +1,27 @@
 import { Controller } from 'stimulus'
+// import google from '@google/maps'
 
-export default class extends Controller {
-  connect(){
-    var map;
-    // console.log(this.element)
-    function initMap() {
-      // console.log(this);
-      map = new google.maps.Map(document.getElementById('map'), {
-      center: {lat: 23.5948979, lng: 119.8960512},
-      zoom: 8
-      });
-    }
-    initMap();
+// window.google = google
+
+// export default class extends Controller {
+//   connect(){
+//     var map;
+//     // console.log(123)
+//     function initMap() {
+//       // console.log(123);
+//       map = new google.maps.Map(document.getElementById('map'), {
+//       center: {lat: 23.5948979, lng: 119.8960512},
+//       zoom: 8
+//       });
+//     }
+//     console.log(123);
+//     initMap();
+//     console.log(456);
 
 
-  }
+//   }
 
 
-}
+// }
+
+
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,7 @@
 import Rails, { $ } from '@rails/ujs'
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
+import google from '@google/maps'
 
 Rails.start()
 ActiveStorage.start()
@@ -16,4 +17,20 @@ import 'channels'
 import 'stylesheets'
 import 'icon'
 import 'datatable'
+// import 'components'
 
+
+document.addEventListener("turbolinks:load",()=>{
+  var map;
+    // console.log(123)
+    function initMap() {
+      // console.log(123);
+      map = new google.maps.Map(document.getElementById('map'), {
+      center: {lat: 23.5948979, lng: 119.8960512},
+      zoom: 8
+      });
+    }
+    // console.log("ho");
+  initMap();
+  // console.log("haha");
+  })

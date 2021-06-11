01d2c20430e67207e3e75739470805a2cc6f57ab
RobertChang <fishppm0212@outlook.com>
2021-06-11 23:21:03 +0800

    done index/slider/fake infomation

--- app/javascript/packs/swiper.js
+++ app/javascript/packs/swiper.js
@@ -2,20 +2,18 @@ import 'swiper/swiper-bundle.css';
 import Swiper from 'swiper/bundle';
 document.addEventListener('turbolinks:load', ()=>{
   const swiper = new Swiper('.swiper-container', {
-    // Optional parameters
     direction: 'horizontal',
     loop: true,
-  
-    // If we need pagination
     pagination: {
       el: '.swiper-pagination',
       clickable: true
     },
-  
-    // Navigation arrows
     navigation: {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
+    },
+    autoplay: {
+      delay: 5000,
     }
   });
 })
